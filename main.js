ef81593622ffaf3c7cf7aecb5260dc6026ef5984
Caroline <carolinebastiaa@gmail.com>
2025-05-05 23:10:12 -0700

    minor changes to changed session expire time

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+const expireTime = 1 * 0 * 0 * 0; //expires after 1 hour  (hours * minutes * seconds * millis)
 
 
 /* secret information section */
